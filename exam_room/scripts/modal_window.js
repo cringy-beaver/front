db2d78bc5d88209965298e71bc53d6177d770940
perceptr <st.arms.all@gmail.com>
2023-06-23 21:05:31 +0500

    Deploy v1

--- exam_room/scripts/modal_window.js
+++ exam_room/scripts/modal_window.js
@@ -1,10 +1,39 @@
+// export function showModalWindow(message, code){
+//     const modal = document.getElementById("error-window");
+//     const modalMessage = document.getElementById("error-window-message");
+//     const modalClose = document.getElementById("error-window-close");
+//     modal.style.display = "block";
+//     modalMessage.innerHTML = (code === 200) ? `Ошибка: ${message}` : "Так просто не убежите." ;
+//     modalClose.addEventListener("click", function() {
+//         modal.style.display = "none";
+//     });
+// }
+
+/**
+ * Show the modal window with a specified message and code
+ * @param {string} message - The message to display in the modal window
+ * @param {number} code - The status code
+ */
 export function showModalWindow(message, code){
-    const modal = document.getElementById("error-window");
-    const modalMessage = document.getElementById("error-window-message");
-    const modalClose = document.getElementById("error-window-close");
+    const modal = document.querySelector("#error-window");
+    const modalMessage = document.querySelector("#error-window-message");
+    const modalClose = document.querySelector("#error-window-close");
+
     modal.style.display = "block";
-    modalMessage.innerHTML = (code === 200) ? `Ошибка: ${message}` : "Так просто не убежите." ;
-    modalClose.addEventListener("click", function() {
+    modalMessage.textContent = (code === 200) ? `Ошибка: ${message}` : "Так просто не убежите." ;
+
+    const closeModal = function() {
         modal.style.display = "none";
-    });
+        modalClose.removeEventListener("click", closeModal);
+        window.removeEventListener('keydown', onEscapeKeyDown);
+    };
+
+    const onEscapeKeyDown = function(e) {
+        if (e.key === 'Escape') {
+            closeModal();
+        }
+    };
+
+    modalClose.addEventListener("click", closeModal);
+    window.addEventListener('keydown', onEscapeKeyDown);
 }
--- load_header.js
+++ load_header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('/front/header.html')
+    fetch('/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
