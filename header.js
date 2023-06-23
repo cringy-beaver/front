f39c110f96440c63003cc791e2e1f22c8c640257
Ilya Kovalchuk <sckremyulock@gmail.com>
2023-06-24 00:17:37 +0500

    fix quit button

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
-// const info = document.querySelector('#info');
-// const user = localStorage.getItem('user');
-// if (user) {
-//     const newParagraph = document.createElement('p');
-//     newParagraph.textContent = `${user['name']} ${user['second_name']}`;
-//     info.appendChild(newParagraph);
-// }
+const info = document.querySelector('#info');
+const user = localStorage.getItem('user');
+if (user !== undefined) {
+    const newH2 = document.createElement('h2');
+    newH2.textContent = `${user['name']} ${user['second_name']}`
+    info.appendChild(newH2);
+}
--- load_header.js
+++ load_header.js
@@ -3,13 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
+            const exitBtn = document.querySelector("#exit");
+            exitBtn.addEventListener('click', clearLocalStorage)
+            const info = document.querySelector('#info');
+            const user = JSON.parse(localStorage.getItem('user'));
+            if (user !== undefined) {
+                const newH2 = document.createElement('h2');
+                newH2.textContent = `${user['name']} ${user['second_name']}`
+                info.insertBefore(newH2, info.firstChild);
+            }
         });
 });
 
-// const exitBtn = document.querySelector("#exit");
-// exitBtn.addEventListener('click', clearLocalStorage())
-//
-// function clearLocalStorage(){
-//     localStorage.clear();
-//     window.location.href = '/front/reg_log/login.html';
-// }
+function clearLocalStorage(){
+    localStorage.clear();
+    window.location.href = '/front/reg_log/login.html';
+}
