02e0f26ff4d382d422abc9a2e21768b80943ca94
StepaZet <stepa.zet2@gmail.com>
2023-06-26 01:22:14 +0500

    mini fixes next gen

--- exam_room/scripts/actions/act_leave_room.js
+++ exam_room/scripts/actions/act_leave_room.js
@@ -11,8 +11,8 @@ export function leaveRoomAction(jsonEvent, room){
         const user = User.fromJson(data.user);
         const thisUser = window.roomEnities['user'];
         if (thisUser.id === user.id){
-            createModal('Комната была закрыта', true, '../main_page/hub.html')
-            window.location.href = '../main_page/hub.html'
+            createModal('Вас исключили из комнаты', true, '../main_page/hub.html')
+            // window.location.href = '../main_page/hub.html'
         }
         room.leaveRoom(user);
     }
